54ce943d999d13693aaafce5e9b55e50fff5c8fb
Olaitan O. Habeeb <olaitan6201@gmail.com>
2022-08-25 23:49:25 +0000

    applied css styles

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,10 +8,37 @@ const title = "Todos"
     <title>{title}</title>
 </svelte:head>
 
-<h1>{title}</h1>
+<div class="todos">
+    <h1>{title}</h1>
+    
+    <form action="" method="" class="new">
+        <input type="text" name="text" aria-label="Add a todo" placeholder="+ type to add a todo" />
+    </form>
+    
+    <TodoItem done />
+    <TodoItem />
+    <TodoItem />
+    <TodoItem />
+</div>
 
-<form action="" method="">
-    <input type="text" name="text" aria-label="Add a todo" placeholder="+ type to add a todo" />
-</form>
+<style>
+    .todos{
+        width: 100%;
+        max-width: 42rem;
+        margin: 4rem auto 0 auto;
+    }
 
-<TodoItem />
+    .new{
+        margin: 0 0 0.5rem 0;
+    }
+
+    .new input{
+        font-size: 28px;
+        width: 100%;
+        padding: .5em 1em .3em 1em;
+        box-sizing: border-box;
+        background-color: rgba(255, 255, 255, .05);
+        border-radius: 8px;
+        text-align: center;
+    }
+</style>
